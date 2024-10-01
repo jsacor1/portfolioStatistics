454f393e5c7165f933dedcf8c694cc7a1923f00c
jsacor1 <jsacor@hotmail.com>
2024-10-01 08:14:26 +0100

    add fontawesome dep and add table placeholder

--- R/shiny.R
+++ R/shiny.R
@@ -1,5 +1,16 @@
 shinyPortfolioStatisticsUI <- function() {
     shiny::tagList(
+        htmltools::htmlDependency(
+            name = "custom_css",
+            version = utils::packageVersion("portfolioStatistics"),
+            src = c(file = system.file(
+                "assets/css/",
+                package = "portfolioStatistics"
+            )),
+            stylesheet = c(
+                "fontawesome.css"
+            )
+        ),
         shiny::div(
             class = "test-primary",
             style = titleStyle,
@@ -20,7 +31,7 @@ shinyPortfolioStatisticsUI <- function() {
             style = titleStyle,
             "Data Overview"
         ),
-        shiny::dataTableOutput(outputId = "dataOverview"),
+        DT::dataTableOutput(outputId = "dataOverview"),
         shiny::div(
             style = titleStyle,
             "Statistics"
@@ -35,5 +46,38 @@ shinyPortfolioStatisticsUI <- function() {
 }
 
 shinyPortfolioStatisticsServer <- function(input, output, session) {
+
+    data <- readxl::read_excel("data-raw/test_data.xlsx")
+
+    output$dataOverview <- DT::renderDataTable({
+        names <- colnames(data)[colnames(data) != "date"]
+        dt <- data.frame(
+            "Name" = names,
+            "# of Observations" = sapply(data[names], function(x) sum(!is.na(x))), # nolint: line_length_linter.
+            "# of CPPI Violations" = 0,
+            "Actions" = as.character(
+                shiny::tags$div(
+                    name = "actions-block-js",
+                    style = "
+                        display: inline-flex;
+                        width: 100%;
+                        justify-content: center
+                    ",
+                    shiny::tags$i(
+                        name = "actions-block-js",
+                        class = "text-primary text-center fas fa-cog"
+                    )
+                )
+            ),
+            check.names = FALSE
+        )
+        DT::datatable(
+            data = dt,
+            selection = "none",
+            class = "primary",
+            escape = FALSE
+        )
+
+    })
 
 }
